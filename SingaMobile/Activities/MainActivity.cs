4554e28ed153798d30f9bd54c65c91a36329479f
Valeriy Posvistak <dynamo.kyiv3001@gmail.com>
2016-12-10 20:39:18 +0200

    slide menu

--- SingaMobile/Activities/MainActivity.cs
+++ SingaMobile/Activities/MainActivity.cs
@@ -1,15 +1,76 @@
 using Android.App;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V4.Widget;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Views;
 
 namespace SingaMobile.Activities
 {
-    [Activity(Label = "MainActivity",Theme = "@style/AppTheme.Dark")]
-    public class MainActivity : Activity
+    [Activity(Label = "MainActivity", Theme = "@style/AppTheme.NoActionBar")]
+    public class MainActivity : AppCompatActivity, NavigationView.IOnNavigationItemSelectedListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            var toggle = new ActionBarDrawerToggle(this, drawer, toolbar, Resource.String.navigation_drawer_open, Resource.String.navigation_drawer_close);
+            toggle.SyncState();
+
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.SetNavigationItemSelectedListener(this);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main, menu);
+            return true;
+        }
+
+        public override void OnBackPressed()
+        {
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            if (drawer.IsDrawerOpen(GravityCompat.Start))
+                drawer.CloseDrawer(GravityCompat.Start);
+            else
+                base.OnBackPressed();
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+
+            if (id == Resource.Id.action_settings)
+                return true;
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            switch (id)
+            {
+                case Resource.Id.nav_profiles:
+                    break;
+                case Resource.Id.nav_messages:
+                    break;
+                case Resource.Id.nav_groups:
+                    break;
+                case Resource.Id.nav_settings:
+                    break;
+            }
+
+            var drawer = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            drawer.CloseDrawer(GravityCompat.Start);
+            return true;
         }
     }
 }
